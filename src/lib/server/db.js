1b9c8ed349f0bf0790ea119f0d1ef77e81f37417
Judith Gammelli <60819967+JGammelli@users.noreply.github.com>
2025-11-13 18:55:39 +0100

    Update db.js

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,4 +1,4 @@
 import 'dotenv/config';
 import { neon } from '@neondatabase/serverless';
 
-export const sql = neon(process.env.DATABASE_URL);
+export const sql = neon(process.env.NETLIFY_DATABASE_URL);
