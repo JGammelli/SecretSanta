9dfd086727e3f3871f0f2014bed8ac6f51b1fbd6
Judith Gammelli <60819967+JGammelli@users.noreply.github.com>
2025-11-13 18:45:32 +0100

    up

--- src/lib/data/projects.js
+++ src/lib/data/projects.js
@@ -1,37 +1,37 @@
-export const projects = [
-  {
-    "id": 1,
-    "title": "Judith",
-    "secretSantaTo": "",
-    "wishList": {},
-    "isPicked": false
-  },
-  {
-    "id": 2,
-    "title": "Elin",
-    "secretSantaTo": "",
-    "wishList": {},
-    "isPicked": false
-  },
-  {
-    "id": 3,
-    "title": "Line",
-    "secretSantaTo": "",
-    "wishList": {},
-    "isPicked": false
-  },
-  {
-    "id": 4,
-    "title": "Stefan",
-    "secretSantaTo": "",
-    "wishList": {},
-    "isPicked": false
-  },
-  {
-    "id": 6,
-    "title": "Lillian och Åke",
-    "secretSantaTo": "",
-    "wishList": {},
-    "isPicked": false
-  }
-];
+// export const projects = [
+//   {
+//     "id": 1,
+//     "title": "Judith",
+//     "secretSantaTo": "",
+//     "wishList": {},
+//     "isPicked": false
+//   },
+//   {
+//     "id": 2,
+//     "title": "Elin",
+//     "secretSantaTo": "",
+//     "wishList": {},
+//     "isPicked": false
+//   },
+//   {
+//     "id": 3,
+//     "title": "Line",
+//     "secretSantaTo": "",
+//     "wishList": {},
+//     "isPicked": false
+//   },
+//   {
+//     "id": 4,
+//     "title": "Stefan",
+//     "secretSantaTo": "",
+//     "wishList": {},
+//     "isPicked": false
+//   },
+//   {
+//     "id": 6,
+//     "title": "Lillian och Åke",
+//     "secretSantaTo": "",
+//     "wishList": {},
+//     "isPicked": false
+//   }
+// ];
